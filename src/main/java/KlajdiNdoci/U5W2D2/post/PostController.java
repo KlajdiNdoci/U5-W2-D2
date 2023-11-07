1dcb4e42f3f99aeb25664e6f7876cacf174bd94e
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-07 15:00:54 +0100

    get posts list

--- src/main/java/KlajdiNdoci/U5W2D2/post/PostController.java
+++ src/main/java/KlajdiNdoci/U5W2D2/post/PostController.java
@@ -1,13 +1,20 @@
 package KlajdiNdoci.U5W2D2.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
     @Autowired
     private PostService postService;
-    
+
+    @GetMapping("")
+    public List<Post> getPost() {
+        return postService.getPosts();
+    }
 }
--- src/main/java/KlajdiNdoci/U5W2D2/post/PostService.java
+++ src/main/java/KlajdiNdoci/U5W2D2/post/PostService.java
@@ -2,6 +2,14 @@ package KlajdiNdoci.U5W2D2.post;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class PostService {
+    private List<Post> posts = new ArrayList<>();
+
+    public List<Post> getPosts() {
+        return this.posts;
+    }
 }
