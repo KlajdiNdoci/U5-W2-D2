4524268e958d8ada1de413635508c6a5cc579db8
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-07 14:26:39 +0100

    basic controllers

---
+++ src/main/java/KlajdiNdoci/U5W2D2/post/PostController.java
@@ -0,0 +1,13 @@
+package KlajdiNdoci.U5W2D2.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    @Autowired
+    private PostService postService;
+    
+}
