51c3a8f3781b7e294263072c70a7a96d7aab3a50
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-07 14:22:44 +0100

    created classes

---
+++ src/main/java/KlajdiNdoci/U5W2D2/post/PostService.java
@@ -0,0 +1,7 @@
+package KlajdiNdoci.U5W2D2.post;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+}
---
+++ src/main/java/KlajdiNdoci/U5W2D2/utente/UtenteService.java
@@ -0,0 +1,7 @@
+package KlajdiNdoci.U5W2D2.utente;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtenteService {
+}
